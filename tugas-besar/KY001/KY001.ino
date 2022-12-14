fe665dc7c5848b3299f9a108f5cc2131767299d5
yisyista <morenzomuten@gmail.com>
2022-12-14 16:23:45 +0700

    Create KY001.ino

---
+++ tugas-besar/KY001/KY001.ino
@@ -0,0 +1,33 @@
+// import needed libraries
+#include <DallasTemperature.h>
+#include <OneWire.h>
+
+// Declaration of the input pin which is connected with the sensor module
+#define KY001_Signal_PIN 4
+
+// libraries configuration
+OneWire oneWire(KY001_Signal_PIN);
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  // serial output initialization
+  Serial.begin(9600);
+  Serial.println("KY-001 temperature measurement");
+  
+  // sensor will be initialized
+  sensors.begin();
+}
+
+//main program loop
+void loop()
+{
+  // temperature measurment will be started...
+  sensors.requestTemperatures();
+  // ... and measured temperature will be displayed
+  Serial.print("Temperature: ");
+  Serial.print(sensors.getTempCByIndex(0));
+  Serial.write(176); // UniCode of the char-symbol "°-Symbol"
+  Serial.println("C");
+  
+  delay(1000); // 1s break till next measurment
+}
