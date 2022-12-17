4af589a6cda5fbae97ceab7484b014a1fb22e640
yisyista <morenzomuten@gmail.com>
2022-12-17 15:20:55 +0700

    added comment

--- tugas-besar/temperature_pid_control/temperature_pid_control.ino
+++ tugas-besar/temperature_pid_control/temperature_pid_control.ino
@@ -1,64 +1,69 @@
-// Temperature Sensor
+/*EB3105 Pengukuran dan Instrumentasi Biomedika
+ *Modul             : Tugas Besar
+ *Hari dan Tanggal  : Sabtu, 17 Desember 2022
+ *Nama (NIM) 1      : Eraraya Morenzo Muten (18320003)
+ *Nama (NIM) 2      : Kayyisa Zahratulfirdaus (18320011)
+ *Nama (NIM) 3      : Indira Mona Farhany (18320040)
+ *Nama (NIM) 4      : Farrel Jonathan Vickeldo (18320008)
+ *Nama File         : temperature_pid_control
+ *Deskripsi         : 
+ *                    
+ *                    
+ */
+
+// Deklarasi Variabel dan Library
+// Sensor temperatur
 #include <DallasTemperature.h>
 #include <OneWire.h>
-#define KY001_Signal_PIN 4
+#define ky001_pin 4
 
-// Solid State Relay
-#define SSR_PIN  11
+// Solid state relay
+#define ssr_pin  11
 
-// Fan PWM
-#define Fan_PWM_PIN 9
-int fanValue = 255; // range 0 - 255
+// Mode inkubator
+uint8_t modes = 0; 
 
-// State
-uint8_t state = 0; 
-
-// Variables
-float real_temp;           //We will store here the real temp 
-float Setpoint = 36;      //In degrees C
-float SetpointDiff = 1;   //In degrees C
-float elapsedTime, now_time, prev_time;        //Variables for time control
-float refresh_rate = 200;                   //PID loop time in ms
-float now_pid_error, prev_pid_error;
-
-
-
-///////////////////PID constants///////////////////////
-float kp=1280.0;         //Mine was 2.5
-float ki=1.0;         //Mine was 0.06
-float kd=0.0;         //Mine was 0.8
-float PID_p, PID_i, PID_d, PID_total;
-int PID_map;
-///////////////////////////////////////////////////////
+// Kontrol PID
+float temp_read;            // Pembacaan temperatur inkubator
+float temp_set = 36;        // Pengaturan temperatur inkubator
+float temp_diff = 1;        // Selisih temperatur untuk memulai mode PID
+float temp_error_now;       // Selisih temperatur pembacaan dengan pengaturan sekarang
+float temp_error_prev;      // Selisih temperatur pembacaan dengan pengaturan sebelumnya
+float time_now;             // Waktu sekarang
+float time_prev;            // Waktu sebelumnya
+float time_passed;          // Selisih waktu sekarang dengan sebelumnya
+float sample_period = 200;  // Perioda pencuplikan PID
+float pid_kp = 2176.0;      // Koefisien komponen proporsional PID
+float pid_ki = 1.7;         // Koefisien komponen integral PID
+float pid_p;                // Nilai komponen proporsional PID
+float pid_i;                // Nilai komponen integral PID
+int pid_total;              // Nilai PID
 
 // libraries configuration
-OneWire oneWire(KY001_Signal_PIN);
+OneWire oneWire(ky001_pin);
 DallasTemperature sensors(&oneWire);
 
 void setup() {
   Serial.begin(9600);
 
   // Solid State Relay
-  pinMode(SSR_PIN, OUTPUT);  
-  digitalWrite(SSR_PIN, HIGH);    // When HIGH, the SSR is Off
+  pinMode(ssr_pin, OUTPUT);  
+  digitalWrite(ssr_pin, HIGH);    // When HIGH, the SSR is Off
   TCCR2B = TCCR2B & B11111000 | B00000111;    // D11 PWM is now 30.64 Hz
 
   // Temperature Sensor
   Serial.println("KY-001 temperature measurement");
   sensors.begin();
   sensors.requestTemperatures();
-  real_temp = (sensors.getTempCByIndex(0))*0.8904+1.2337;
-  Serial.print(real_temp);
-  
-  // Fan PWM
-  analogWrite(Fan_PWM_PIN, fanValue);
+  temp_read = (sensors.getTempCByIndex(0))*0.8904+1.2337;
+  Serial.print(temp_read);
 }
 
 void loop() {  
-  if(state == 0){
+  if(modes == 0){
     ramp_up(); 
   }
-  else if (state == 1){
+  else if (modes == 1){
     PID_control(); 
   }
   else{
@@ -69,102 +74,99 @@ void loop() {
 //Fucntion for ramping up the temperature
 //Function buat nyalain SSR/heater kalau suhunya masih kurang
 void ramp_up(void){  
-  //Rising temperature to (Setpoint - SetpointDiff)
-  elapsedTime = millis() - prev_time; 
-  if(elapsedTime > refresh_rate){  
+  //Rising temperature to (temp_set - temp_diff)
+  time_passed = millis() - time_prev; 
+  if(time_passed > sample_period){  
     //Baca Nilai
     sensors.requestTemperatures();
-    real_temp = (sensors.getTempCByIndex(0))*0.8904+1.2337;
-    real_temp = (float)round(real_temp*10)/10;
+    temp_read = (sensors.getTempCByIndex(0))*0.8904+1.2337;
+    temp_read = (float)round(temp_read*10)/10;
     
-    if(real_temp < (Setpoint - SetpointDiff)){
-      digitalWrite(SSR_PIN, LOW);                //Turn On SSR
+    if(temp_read < (temp_set - temp_diff)){
+      digitalWrite(ssr_pin, LOW);                //Turn On SSR
     }
     else
     {
-      digitalWrite(SSR_PIN, HIGH);                 //Turn Off SSR
-      state = 1;                                  //Already hot so we go to PID control
+      digitalWrite(ssr_pin, HIGH);                 //Turn Off SSR
+      modes = 1;                                  //Already hot so we go to PID control
     }
 
     Serial.println("Ramp Up");
     //Serial.print("Set: "); 
-    //Serial.println(Setpoint,2);
+    //Serial.println(temp_set,2);
     
     //Serial.print((char)247); 
     //Serial.print("C: "); 
-    Serial.println(real_temp,1);     
+    Serial.println(temp_read,1);     
        
     Serial.println();
     
-    prev_time = millis();
+    time_prev = millis();
   }
 }//End of ramp_up loop
 
 //Main PID compute and execute function
 void PID_control(void){
 /*
-  if(real_temp < (Setpoint - SetpointDiff)){
-      digitalWrite(SSR_PIN, LOW);                //Turn On SSR
-      state = 0;
+  if(temp_read < (temp_set - temp_diff)){
+      digitalWrite(ssr_pin, LOW);                //Turn On SSR
+      modes = 0;
     }
     else
     {  
     */
-      elapsedTime = millis() - prev_time;   
-      if(elapsedTime > refresh_rate){    
+      time_passed = millis() - time_prev;   
+      if(time_passed > sample_period){    
         //1. We get the temperature and calculate the error
         //Baca Nilai
         sensors.requestTemperatures();
-        real_temp = (sensors.getTempCByIndex(0))*0.8904+1.2337;
-        real_temp = (float)round(real_temp*10)/10;
+        temp_read = (sensors.getTempCByIndex(0))*0.8904+1.2337;
+        temp_read = (float)round(temp_read*10)/10;
         
-        now_pid_error = Setpoint - real_temp;
+        temp_error_now = temp_set - temp_read;
       
         //2. We calculate PID values
-        PID_p = kp * now_pid_error;
-        PID_d = kd*((now_pid_error - prev_pid_error)/refresh_rate);
+        pid_p = pid_kp * temp_error_now;
         //2.2 Decide if we apply I or not. Only when error is very small
-        //if((-0.2 < now_pid_error) && (now_pid_error < 0.2)){
-          PID_i = PID_i + (ki * now_pid_error);//}
-        //else{PID_i = 0;}
+        //if((-0.2 < temp_error_now) && (temp_error_now < 0.2)){
+          pid_i = pid_i + (pid_ki * temp_error_now);//}
+        //else{pid_i = 0;}
     
         //3. Calculate and map total PID value
-        PID_total = PID_p + PID_i + PID_d;  
-        PID_map = int(map(PID_total, 0, 150, 0, 255));
+        pid_total = floor(pid_p + pid_i);  
+        pid_total = constrain(pid_total, 0, 255);
     
-        //4. Set limits for PID values
-        if(PID_map < 0){PID_map = 0;}
-        if(PID_map > 255) {PID_map = 255; } 
+//        //4. Set limits for PID values
+//        if(PID_map < 0){PID_map = 0;}
+//        if(PID_map > 255) {PID_map = 255; } 
     
         //5. Write PWM signal to the SSR
-        analogWrite(SSR_PIN, 255-PID_map);
+        analogWrite(ssr_pin, 255-pid_total);
     
-        Serial.print("PID map: ");  
-        Serial.print(PID_map);
+        Serial.print("PID PWM: ");  
+        Serial.print(255-pid_total);
         Serial.print(", p: ");
-        Serial.print(PID_p);
-        Serial.print(", d: ");
-        Serial.print(PID_d);
+        Serial.print(pid_p);
         Serial.print(", i: ");
-        Serial.print(PID_i);
+        Serial.print(pid_i);
         Serial.print(", total: ");
-        Serial.println(PID_total);
+        Serial.println(pid_total);
     
         //Serial.print("Set: "); 
-        //Serial.println(Setpoint,2);
+        //Serial.println(temp_set,2);
         
         //Serial.print((char)247); 
         //Serial.print("C: "); 
-        Serial.println(real_temp,1);  
+        Serial.println(temp_read,1);  
         
         Serial.println();
         //Serial.display();//Finally display the created image
         
         
         //7. Save values for next loop
-        prev_time = millis();                       //Store time for next loop
-        prev_pid_error = now_pid_error;             //Store error for next loop
-        //Serial.println(elapsedTime);                //For debug only
+        time_prev = millis();                       //Store time for next loop
+        temp_error_prev = temp_error_now;             //Store error for next loop
+        //Serial.println(time_passed);                //For debug only
         
         //LED_State = !LED_State;
         //digitalWrite(led, LED_State);
@@ -180,16 +182,16 @@ void PID_control(void){
 
 //Function for turning off everything and monitoring the coolidn down process
 void cool_down(void){
-  digitalWrite(SSR_PIN, HIGH);    //SSR is OFF with HIGH pulse!
+  digitalWrite(ssr_pin, HIGH);    //SSR is OFF with HIGH pulse!
   //digitalWrite(led, LOW);
-  elapsedTime = millis() - prev_time;   
-  if(elapsedTime > refresh_rate){ 
+  time_passed = millis() - time_prev;   
+  if(time_passed > sample_period){ 
 
     Serial.println("Off");     
     Serial.print("Set: "); 
-    Serial.println(Setpoint,2);
+    Serial.println(temp_set,2);
        
     //Serial.display();//Finally display the created image
-    prev_time = millis();
+    time_prev = millis();
   }
 }//End cool_down loop
