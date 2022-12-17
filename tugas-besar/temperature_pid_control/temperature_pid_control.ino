d922f177a8039f639e5e8a715fe1ac79c278a214
yisyista <morenzomuten@gmail.com>
2022-12-17 22:13:19 +0700

    added overshoot and undershoot handling

--- tugas-besar/temperature_pid_control/temperature_pid_control.ino
+++ tugas-besar/temperature_pid_control/temperature_pid_control.ino
@@ -22,15 +22,18 @@ DallasTemperature sensors(&oneWire);
 // Solid state relay
 #define ssr_pin  11
 
+// Fan PWM
+#define fan_pin 9
+
 // Mode inkubator
-uint8_t modes = 2; 
+uint8_t modes = 0; 
 
 // Kontrol PID
 float temp_read;            // Pembacaan temperatur inkubator (drjtC)
 float temp_set = 36;        // Pengaturan temperatur inkubator (drjtC)
 float temp_diff = 1;        // Selisih temperatur untuk memulai mode PID (drjtC)
-float temp_error_now;       // Selisih temperatur pembacaan dengan pengaturan sekarang (drjtC)
-float temp_error_prev;      // Selisih temperatur pembacaan dengan pengaturan sebelumnya (drjtC)
+float temp_error;           // Selisih temperatur pembacaan dengan pengaturan (drjtC)
+float temp_start;           // Pembacaan temperatur pertama (drjtC)
 float time_now = 0;         // Waktu sekarang (ms)
 float time_prev = 0;        // Waktu sebelumnya (ms)
 float time_passed;          // Selisih waktu sekarang dengan sebelumnya (ms)
@@ -38,7 +41,7 @@ float sample_period = 1010; // Perioda pencuplikan PID (ms)
 float pid_kp = 2560.0;      // Koefisien komponen proporsional PID
 float pid_ki = 1.0;         // Koefisien komponen integral PID
 float pid_p;                // Nilai komponen proporsional PID
-float pid_i;                // Nilai komponen integral PID
+float pid_i = 0;            // Nilai komponen integral PID
 int pid_total;              // Nilai PID
 
 void setup() {
@@ -52,28 +55,44 @@ void setup() {
 
   // Memulai pembacaan temperatur
   sensors.begin();
+  // Menyimpan temperatur awal
+  sensors.requestTemperatures();
+  temp_start = (sensors.getTempCByIndex(0))*0.8904+1.2337;
+  temp_start = (float)round(temp_start*10)/10;
 }
 
 void loop() {
   if(Serial.available()) {
       byte command=Serial.read();
       if(command==110) {      // 'n'
-        modes = 0;
+        // Memasuki mode rise
+        modes = 1;
+        // Menyalakan Kipas
+        digitalWrite(fan_pin, HIGH);
+        // Menyimpan temperatur awal
+        sensors.requestTemperatures();
+        temp_start = (sensors.getTempCByIndex(0))*0.8904+1.2337;
+        temp_start = (float)round(temp_start*10)/10;        
         //Serial.println("on");
       } else if (command==102) { // 'f'
-        modes = 2;
+        // Memasuki mode shutdown
+        modes = 4;
         //Serial.println("off");
       }
   }
   
+  // Mode off
   if(modes == 0){
+    digitalWrite(ssr_pin, HIGH);                 //Turn Off SSR
+    digitalWrite(fan_pin, HIGH);                 //Turn Off fan
+  } else if (modes == 1){
     mode_rise(); 
-  }
-  else if (modes == 1){
+  } else if (modes == 2){
     mode_pid(); 
-  }
-  else if (modes == 2){
+  } else if (modes == 3){
     mode_fall(); 
+  } else if (modes == 4){
+    mode_shutdown(); 
   }
 }
 
@@ -94,10 +113,10 @@ void mode_rise(void){
     else
     {
       digitalWrite(ssr_pin, HIGH);                 //Turn Off SSR
-      modes = 1;                                  //Already hot so we go to PID control
+      modes = 2;                                  //Already hot so we go to PID control
     }
 
-    Serial.println("Ramp Up");
+    Serial.println("Rise");
     Serial.println(temp_read,1);     
     Serial.println();
     
@@ -116,11 +135,11 @@ void mode_pid(void){
     temp_read = (sensors.getTempCByIndex(0))*0.8904+1.2337;
     temp_read = (float)round(temp_read*10)/10;
     
-    temp_error_now = temp_set - temp_read;
+    temp_error = temp_set - temp_read;
   
     //2. We calculate PID values
-    pid_p = pid_kp * temp_error_now;
-    pid_i = pid_i + (pid_ki * temp_error_now);
+    pid_p = pid_kp * temp_error;
+    pid_i = pid_i + (pid_ki * temp_error);
 
     //3. Calculate and map total PID value
     pid_total = floor(pid_p + pid_i);  
@@ -143,25 +162,68 @@ void mode_pid(void){
     
     //7. Save values for next loop
     time_prev = time_now;                       //Store time for next loop
-    temp_error_prev = temp_error_now;             //Store error for next loop
+
+    // Kembali ke mode rise jika turun terlalu jauh
+    if (temp_read <= (temp_set - (temp_diff+0.5))){
+      modes = 1;
+      // Reset nilai komponen integral PID
+      pid_i = 0;
+    } else if (temp_read >= (temp_set + (temp_diff+0.5))){
+      modes = 3;
+      // Reset nilai komponen integral PID
+      pid_i = 0;
+    }
   }  
 }
 
 //Function for turning off everything and monitoring the coolidn down process
 void mode_fall(void){
-  digitalWrite(ssr_pin, HIGH);    //SSR is OFF with HIGH pulse!
-  //digitalWrite(led, LOW);
   time_now = millis();
-  time_passed = time_now - time_prev;   
-  if(time_passed > sample_period){ 
-
+  time_passed = time_now - time_prev; 
+  if(time_passed > sample_period){  
     //Baca Nilai
     sensors.requestTemperatures();
-    temp_read = (sensors.getTempCByIndex(0))*0.8904+1.2337; 
+    temp_read = (sensors.getTempCByIndex(0))*0.8904+1.2337;
     temp_read = (float)round(temp_read*10)/10;
     
-    Serial.println("Off");     
-    Serial.println(temp_read,1);
+    if(temp_read > (temp_set + temp_diff)){
+      digitalWrite(ssr_pin, HIGH);                //Turn Off SSR
+    }
+    else
+    {
+      digitalWrite(ssr_pin, LOW);                 //Turn On SSR
+      modes = 2;                                  //Already cold so we go to PID control
+    }
+
+    Serial.println("Fall");
+    Serial.println(temp_read,1);     
+    Serial.println();
+    
+    time_prev = time_now;
+  }
+}//End cool_down loop
+
+void mode_shutdown(void){
+  time_now = millis();
+  time_passed = time_now - time_prev; 
+  if(time_passed > sample_period){  
+    //Baca Nilai
+    sensors.requestTemperatures();
+    temp_read = (sensors.getTempCByIndex(0))*0.8904+1.2337;
+    temp_read = (float)round(temp_read*10)/10;
+    
+    if(temp_read > (temp_start)){
+      digitalWrite(ssr_pin, HIGH);                //Turn Off SSR
+    }
+    else
+    {
+      digitalWrite(ssr_pin, HIGH);                 //Turn Off SSR
+      digitalWrite(fan_pin, HIGH);                 //Turn Off fan
+      modes = 0;                                  //Already cold so we turn off
+    }
+
+    Serial.println("Shutdown");
+    Serial.println(temp_read,1);     
     Serial.println();
     
     time_prev = time_now;
